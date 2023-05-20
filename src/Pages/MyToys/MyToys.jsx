bf4cabcf5997fc32feae4e21f2ba127b7f7c3235
Abid Hasan <108365863+AbidReal@users.noreply.github.com>
2023-05-20 20:49:58 +0600

    delete confirmation modal done

--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -17,6 +17,8 @@ const MyToys = () => {
   };
   const { user } = useContext(AuthContext);
   const [myToys, setMyToys] = useState([]);
+  const [confirmModalOpen, setConfirmModalOpen] = useState(false);
+  const [toyToDelete, setToyToDelete] = useState(null);
 
   const url = `http://localhost:5000/toys?email=${user.email}&sort=1`;
   useEffect(() => {
@@ -25,13 +27,17 @@ const MyToys = () => {
       .then((data) => setMyToys(data));
   }, []);
 
+  const handleDelete = (toyId) => {
+    setToyToDelete(toyId);
+    setConfirmModalOpen(true);
+  };
+
   const handleEdit = (toyId) => {
     console.log(`Edit toy with ID: ${toyId}`);
   };
-  const handleDelete = (toyId) => {
-    const proceed = confirm("Are you sure you want to delete");
-    if (proceed) {
-      fetch(`http://localhost:5000/toys/${toyId}`, {
+  const handleConfirmDelete = () => {
+    if (toyToDelete) {
+      fetch(`http://localhost:5000/toys/${toyToDelete}`, {
         method: "DELETE",
       })
         .then((res) => res.json())
@@ -39,12 +45,15 @@ const MyToys = () => {
           console.log(data);
           if (data.deletedCount > 0) {
             handleToast();
-            const remaining = myToys.filter((toy) => toy._id !== toyId);
+            const remaining = myToys.filter((toy) => toy._id !== toyToDelete);
             setMyToys(remaining);
           }
+        })
+        .finally(() => {
+          setToyToDelete(null);
+          setConfirmModalOpen(false);
         });
     }
-    console.log(`delete toy id: ${toyId}`);
   };
   return (
     <div className="custom-container text-center">
@@ -93,6 +102,62 @@ const MyToys = () => {
           )}
         </tbody>
       </table>
+      {/* Confirm Delete Modal */}
+      {confirmModalOpen && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+            <div className="fixed inset-0 transition-opacity">
+              <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+            </div>
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
+            &#8203;
+            <div
+              className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+              role="dialog"
+              aria-modal="true"
+              aria-labelledby="modal-headline"
+            >
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <FaTrashAlt className="h-6 w-6 text-red-600" />
+                  </div>
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3
+                      className="text-lg leading-6 font-medium text-gray-900"
+                      id="modal-headline"
+                    >
+                      Delete Toy
+                    </h3>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        Are you sure you want to delete this toy?
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={handleConfirmDelete}
+                >
+                  Delete
+                </button>
+                <button
+                  type="button"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+                  onClick={() => setConfirmModalOpen(false)}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
       <ToastContainer />
     </div>
   );
