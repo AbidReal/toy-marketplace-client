0a1831912f32138074575abb31cef3e5f455482a
Abid Hasan <108365863+AbidReal@users.noreply.github.com>
2023-05-21 08:26:39 +0600

    search by name done

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const AllToys = () => {
+  const [searchText, setSearchText] = useState([]);
   const [toys, setToys] = useState([]);
   useEffect(() => {
     fetch("http://localhost:5000/toys")
@@ -9,11 +10,55 @@ const AllToys = () => {
       .then((result) => {
         setToys(result);
       });
-  });
+  }, []);
+
+  const handleSearch = () => {
+    fetch(`http://localhost:5000/toySearchByName/${searchText}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setToys(result);
+      });
+  };
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
   return (
     <div>
-      all toys section
       <div className="custom-container">
+        <div className=" flex justify-end mt-5 mb-10 ">
+          <div className="form-control">
+            <div className="input-group">
+              <input
+                onChange={(e) => setSearchText(e.target.value)}
+                onKeyDown={handleKeyPress}
+                type="text"
+                placeholder="Search…"
+                className="input input-bordered bg-black "
+              />
+              <button
+                onClick={handleSearch}
+                className="btn btn-square btn-color "
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
         <table className="table-auto w-full text-xl ">
           <thead>
             <tr>
